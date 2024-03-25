e910d3b87146d86e1a7c91480d598a3f882d0989
Hah-nna <hj08hn@gmail.com>
2024-03-26 00:20:55 +0900

    ✨ feat: routes
    - react0router-dom v6를 사용해 라우팅을 설정합니다

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import "./index.css";
 import Root from "./routes/Root";
 import ErrorPage from "./routes/ErrorPage";
 import { RecoilRoot } from "recoil";
+import Main from "./pages/Main";
+import KakaoAuth from "./routes/KakaoAuth";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Main />,
+          errorElement: <ErrorPage />,
+        },
+      ],
+    },
+    {
+      path: "/kakao-auth",
+      element: <KakaoAuth />,
       errorElement: <ErrorPage />,
     },
   ]);
