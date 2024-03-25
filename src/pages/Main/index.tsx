2abd7d48ce7d9ff6b903a5b6b43a348de0e2ba04
Hah-nna <hj08hn@gmail.com>
2024-03-26 00:30:42 +0900

    ✨feat: kakao login test
    - 카카오 로그인을 위해 테스트중입니다
    - 아직 테스트 완료되지 않았습니다
    
    by Hah-nna #2

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import KakaoLogin from "../../components/button/KakaoLogin";
+
+function Main() {
+  return (
+    <div>
+      <p>메인입니다</p>
+      <KakaoLogin />
+    </div>
+  );
+}
+
+export default Main;
