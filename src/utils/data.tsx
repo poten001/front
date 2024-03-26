987fc4bbd280a6bdb81c8986932dae2b12e1d658
Hah-nna <hj08hn@gmail.com>
2024-03-27 05:47:20 +0900

    ✨ chore: temporary data for carousel
    - 캐러셀에 보여질 임시데이터와 타입을 지정합니다
    
    by Hah-nna

---
+++ src/utils/data.tsx
@@ -0,0 +1,51 @@
+export const sliderMockData = [
+  {
+    id: 23,
+    title: "건강챙기기",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "만보걷기 등",
+  },
+  {
+    id: 23654,
+    title: "음식",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "어쩌구",
+  },
+  {
+    id: 23656,
+    title: "자개개발",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "어쩌구",
+  },
+  {
+    id: 23000,
+    title: "여행/탐험",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "저쩌구",
+  },
+  {
+    id: 234832904,
+    title: "교류",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "샬라샬라",
+  },
+  {
+    id: 2307438,
+    title: "재미",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "밥내놔",
+  },
+  {
+    id: 2302489373284,
+    title: "랜덤",
+    img_url:
+      "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
+    content: "자고싶어요",
+  },
+];
