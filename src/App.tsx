e0f3e5dda2d3f586fed9d77f7ce831aeb35b3047
Hah-nna <hj08hn@gmail.com>
2024-03-28 06:41:47 +0900

    ✨ chore: add routes
    - 라우팅을 추가합니다
    
    by Hah-nna

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import Main from "./pages/Main";
 import KakaoAuth from "./routes/KakaoAuth";
 import LoginPage from "./pages/LoginPage";
 import SettingPage from "./pages/SettingPage";
+import AboutOchaelWanPage from "./pages/SettingPage/About";
+import CreateChallengePage from "./pages/CreateChallengePage";
+import Mypage from "./pages/MyPage";
+import IsChallengeDonePage from "./pages/MyPage/IsChallengeDonePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -25,8 +29,39 @@ function App() {
           element: <SettingPage />,
           errorElement: <ErrorPage />,
         },
+        {
+          path: "/about-ochaelwan",
+          element: <AboutOchaelWanPage />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "/terms-of-service",
+          element: <AboutOchaelWanPage />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "/private-policy",
+          element: <AboutOchaelWanPage />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "/my-page/:id",
+          element: <Mypage />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "is-challenge-done",
+          element: <IsChallengeDonePage />,
+          errorElement: <ErrorPage />,
+        },
       ],
     },
+    {
+      path: "/create-challenge",
+      element: <CreateChallengePage />,
+      errorElement: <ErrorPage />,
+    },
+
     {
       path: "/login",
       element: <LoginPage />,
