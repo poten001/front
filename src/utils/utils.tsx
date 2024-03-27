bbe85402c53df1091e36ef212c25db8c6bdb1300
Hah-nna <hj08hn@gmail.com>
2024-03-28 06:28:00 +0900

    ✨ chore: create date utils
    - 현재 시간, 연도, 날짜, 분, 초를 알 수 있는 유틸함수를 만듭니다
    
    by Hah-nna

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,1 +1,22 @@
 // 시간 추출
+
+const getCurrentDateTime = () => {
+  const currentTime = new Date();
+  const currentYear = currentTime.getFullYear();
+  const currentMonth = currentTime.getMonth() + 1; // getMonth는 0부터 시작하므로 1을 더해줍니다.
+  const currentDate = currentTime.getDate(); // 날짜를 얻기 위해 getDate를 사용합니다. getDay는 요일을 반환합니다.
+  const currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+  const currentSecond = currentTime.getSeconds();
+
+  return {
+    currentYear,
+    currentMonth,
+    currentDate,
+    currentHour,
+    currentMinute,
+    currentSecond,
+  };
+};
+
+export default getCurrentDateTime;
