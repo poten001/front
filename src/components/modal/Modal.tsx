eac5bfec201b9495e45686c84efe1b1904c5211f
Hah-nna <hj08hn@gmail.com>
2024-03-28 06:38:57 +0900

    ✨ feat: create modal
    - 모달을 전역에서 사용할 수 있게 전역상태를 설정하고 props도 두어 모달마다 다른 설정을 사용할 수 있게 합니다
    - portal을 사용해 최상위에서 모달이 열리게끔 합니다
    
    by Hah-nna #1

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,45 @@
-const Modal = () => {
-  return <div>Modal</div>;
+type ModalT = {
+  title: string;
+  subTitle?: string;
+  showCancelBtn?: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+};
+const Modal = ({
+  title,
+  subTitle,
+  showCancelBtn,
+  onConfirm,
+  onCancel,
+}: ModalT) => {
+  return (
+    <div className="fixed w-full h-full bg-[rgba(142,142,142,0.4)] z-20 top-0 left-0 flex justify-center items-center">
+      <div className="bg-white w-[288px] h-[166px] z-30 rounded-xl">
+        <div className="flex flex-col justify-center items-center py-8 px-[62.5px] gap-2">
+          <div className="text-[17px] font-semibold">{title}</div>
+          <div className="text-[13px] text-[#777777]">{subTitle}</div>
+        </div>
+        <div className="flex flex-row border-t border-[#E3E3E3] justify-around text-center">
+          {showCancelBtn && (
+            <button
+              onClick={onCancel}
+              className="flex justify-center items-center w-1/2 h-12 border-r text-[#777777] border-[#E3E3E3]"
+            >
+              취소
+            </button>
+          )}
+          <button
+            onClick={onConfirm}
+            className={`flex justify-center items-center h-12 text-secondary-500 font-bold ${
+              showCancelBtn ? "w-1/2" : "w-full"
+            }`}
+          >
+            확인
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
