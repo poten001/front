b7b78067b382dfeb0aa39c15ce8375d2d658dea1
Hah-nna <hj08hn@gmail.com>
2024-03-28 16:06:36 +0900

    💄design: add loading page - 로딩페이지를 추가합니다
    by Hah-nna #1

--- src/components/Loader/Loading.tsx
+++ src/components/Loader/Loading.tsx
@@ -1,5 +1,14 @@
+import StatusBarLayout from "../layout/StatusBarLayout";
+import LoadingLogo from "../../assets/loading.svg?react";
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <StatusBarLayout showButton={false} color="primary-500">
+      <div className="bg-primary-500 w-full h-full justify-center items-center">
+        <LoadingLogo />
+      </div>
+    </StatusBarLayout>
+  );
 };
 
 export default Loading;
