a9a8175a0e1035f557a6c23c70caf74e91348ac3
Hah-nna <hj08hn@gmail.com>
2024-03-30 03:17:47 +0900

    ✨ chore: login custom hook
    - 로그인 상태를 감지하는 커스텀 훅을 만듭니다
    
    by Hah-nna #14

--- src/hooks/useLoggedIn.tsx
+++ src/hooks/useLoggedIn.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useRecoilState } from "recoil";
+import { loginState } from "../store/loginState";
+
 const useLoggedIn = () => {
   // 카카오 로그인을 위한 커스텀 훅
+
+  const [login, setLogin] = useRecoilState(loginState);
+
+  useEffect(() => {
+    const accessToken = Cookies.get("accessToken");
+
+    if (accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }, []);
+  return { login, setLogin };
 };
 
 export default useLoggedIn;
