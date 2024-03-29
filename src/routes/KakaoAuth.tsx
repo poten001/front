1e4675ba525b2e7abacc6a08b42a6394cc7e2a8d
Hah-nna <hj08hn@gmail.com>
2024-03-30 03:25:38 +0900

    ✨ feat: kakao login 인가코드
    - 카카오 로그인을 위해 인가코드를 받아 보냅니다
    
    by Hah-nna #2

--- src/routes/KakaoAuth.tsx
+++ src/routes/KakaoAuth.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-// import { setCookie } from "../utils/utils";
+import { instance } from "../axios/axios";
+import Cookies from "js-cookie";
 const KakaoAuth = () => {
   // 인가코드 받아서 보내기
 
@@ -17,13 +17,15 @@ const KakaoAuth = () => {
 
     const postKakaoCode = async () => {
       try {
-        const response = await axios
+        await instance
           .post("https://today-challenge.site/auth/login", body)
-          .then(() => {
+          .then((response) => {
             console.log("response", response);
-            // const { accessToken, refreshToken } =
-            // setCookie("accessToken", accessToken);
-            // setCookie("refreshToken", refreshToken);
+
+            const { accessToken, refreshToken } = response.data;
+            console.log("response.data", response.data);
+            Cookies.set("accessToken", accessToken);
+            Cookies.set("refreshToken", refreshToken);
             navigate("/");
           });
       } catch (error) {
