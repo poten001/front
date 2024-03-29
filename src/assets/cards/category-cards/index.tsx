82b727cf1336e74f1479218df61e93a8e8468ebb
Hah-nna <hj08hn@gmail.com>
2024-03-30 03:14:22 +0900

    ✨ chore: add  category image array data
    - 카테고리 이미지를 배열로 모아놓았습니다
    
    by Hah-nna #14

---
+++ src/assets/cards/category-cards/index.tsx
@@ -0,0 +1,38 @@
+import categoryOne from "../category-cards/card-small-01.png";
+import categoryTwo from "../category-cards/card-small-02.png";
+import categoryThree from "../category-cards/card-small-03.png";
+import categoryFour from "../category-cards/card-small-04.png";
+import categoryFive from "../category-cards/card-small-05.png";
+import categorySix from "../category-cards/card-small-06.png";
+import categorySeven from "../category-cards/card-small-07.png";
+
+export const images = [
+  {
+    id: "랜덤",
+    src: categoryOne,
+  },
+  {
+    id: "일상의 재미",
+    src: categoryTwo,
+  },
+  {
+    id: "소통하기",
+    src: categoryThree,
+  },
+  {
+    id: "훌쩍 떠나기",
+    src: categoryFour,
+  },
+  {
+    id: "취미의 발견",
+    src: categoryFive,
+  },
+  {
+    id: "맛있는 도전",
+    src: categorySix,
+  },
+  {
+    id: "건강 챙기기",
+    src: categorySeven,
+  },
+];
