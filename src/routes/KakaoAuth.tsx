2342c82c24b97c3d6eaf16549846f24c3480dff5
Hah-nna <hj08hn@gmail.com>
2024-03-29 10:14:18 +0900

    ✨ feat: kakao login redirect uri
    - 리다이렉트 uri에 있을 때 인가코드를 받은 후 성공했다면 메인, 아니면 에러페이지로 이동합니다
    
    by Hah-nna #2

--- src/routes/KakaoAuth.tsx
+++ src/routes/KakaoAuth.tsx
@@ -1,4 +1,39 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+// import { setCookie } from "../utils/utils";
 const KakaoAuth = () => {
+  // 인가코드 받아서 보내기
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+
+    const body = {
+      authorizationCode: code,
+    };
+    console.log("body", body);
+
+    const postKakaoCode = async () => {
+      try {
+        const response = await axios
+          .post("https://today-challenge.site/auth/login", body)
+          .then(() => {
+            console.log("response", response);
+            // const { accessToken, refreshToken } =
+            // setCookie("accessToken", accessToken);
+            // setCookie("refreshToken", refreshToken);
+            navigate("/");
+          });
+      } catch (error) {
+        console.log("error", error);
+        navigate("/error");
+      }
+    };
+    postKakaoCode();
+  }, []);
+
   return <div>Kakao-auth 로그인 중</div>;
 };
 
