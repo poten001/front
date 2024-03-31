0e4ee82193f04f065602de1756c9b7ed34b25e95
Hah-nna <hj08hn@gmail.com>
2024-03-31 16:31:05 +0900

    ✨ chore: 완료한 챌린지 페이지
     - 완료한 챌린지 페이지에 완료한 시간을 파싱해서 추가합니다
     by Hah-nna #14

--- src/pages/MyPage/doneChallenge.tsx
+++ src/pages/MyPage/doneChallenge.tsx
@@ -1,25 +1,32 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import StatusBarLayout from "../../components/layout/StatusBarLayout";
 import { useNavigate, useParams } from "react-router-dom";
 import html2canvas from "html2canvas";
 import saveAs from "file-saver";
-import { useRecoilValue } from "recoil";
-import { userDataState } from "../../store/userDataState";
 import DownloadIcon from "../../assets/icons/downloadIcon.svg?react";
 import DownArrowIcon from "../../assets/icons/downArrow.svg?react";
 import TimerIcon from "../../assets/icons/timerIcon.svg?react";
-import getCurrentDateTime from "../../utils/utils";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
 
 const DoneChallenge = () => {
   const navigate = useNavigate();
-  const userData = useRecoilValue(userDataState);
+
   const divRef = useRef<HTMLDivElement>(null);
-  const { currentYear, currentMonth, currentDate, currentHour, currentMinute } =
-    getCurrentDateTime();
   const accessToken = Cookies.get("accessToken");
   const { id } = useParams();
+  const [detailData, setDetailData] = useState({
+    id: "",
+    memberName: "",
+    memberProfile: "",
+    startTime: "",
+    completeTime: "",
+    takenTime: "",
+    challengeTitle: "",
+    challengeImg: "",
+  });
 
   useEffect(() => {
     const detailChallengeHandler = async () => {
@@ -27,13 +34,14 @@ const DoneChallenge = () => {
         .get(`https://today-challenge.site/challenge/${id}`, {
           headers: { Authorization: `Bearer ${accessToken}` },
         })
-        .then(() => {
-          console.log("완료");
+        .then((res) => {
+          console.log("res.data doneChallenge", res.data);
+          setDetailData(res.data);
         });
     };
 
     detailChallengeHandler();
-  }, []);
+  }, [id, accessToken]);
 
   const handleDownload = async () => {
     if (!divRef.current) return;
@@ -56,12 +64,26 @@ const DoneChallenge = () => {
     navigate("/");
   };
 
+  const formattedDate = detailData.startTime
+    ? format(new Date(detailData.startTime), "yyyy년 MM월 dd일 HH:mm", {
+        locale: ko,
+      })
+    : "시작 시간 없음";
+
+  const formattedCompleteDate = detailData.completeTime
+    ? format(new Date(detailData.completeTime), "yyyy년 MM월 dd일 HH:mm", {
+        locale: ko,
+      })
+    : "완료 시간 없음";
+
+  console.log("formattedDate", formattedDate);
+  console.log("formattedCompleteDate", formattedCompleteDate);
   return (
     <StatusBarLayout
       color="white"
       showButton={true}
       leftBtnText="취소"
-      rightBtnText="완료"
+      rightBtnText="저장하기"
       leftBtnClick={() => navigate(-1)}
       rightBtnClick={downloadNavigateHandler}
     >
@@ -74,20 +96,20 @@ const DoneChallenge = () => {
         </div>
         <div className="pt-[32px] relative">
           <img
-            src={userData.challengeImg}
+            src={detailData.challengeImg}
             alt="test"
-            className="w-[305px] h-[385px]"
+            className="w-[305px] h-[385px] rounded-xl shadow-xl"
           />
           <div className="absolute left-[30px] top-[68px] flex">
             <img
-              src={userData.memberProfile}
+              src={detailData.memberProfile}
               alt="profile"
               className=" w-[34px] h-[34px] rounded-full"
             />
 
             <div className="">
-              <h3 className="font-semibold ">{userData.memberName}</h3>
-              <p className="body-s text-[#4B4B4B]">{`${currentYear}년 ${currentMonth}월 ${currentDate}일 ${currentHour}:${currentMinute}`}</p>
+              <h3 className="font-semibold ">{detailData.memberName}</h3>
+              <p className="body-s text-[#4B4B4B]">{formattedDate}</p>
             </div>
           </div>
           <div className="absolute top-[64px] cursor-pointer right-8 ">
@@ -97,13 +119,16 @@ const DoneChallenge = () => {
               onClick={downloadNavigateHandler}
             />
           </div>
+          <div className="body-l absolute bottom-[30px] px-5 w-full  h-[50px] left-0 right-0 m-auto text-center text-wrap break-keep">
+            {detailData.challengeTitle}
+          </div>
         </div>
         <div className="flex flex-col pt-[52px]  justify-center items-center gap-[14px]">
           <DownArrowIcon />
-          <h2>{userData.expireTime}</h2>
+          <h2>{detailData.takenTime}</h2>
           <div className="flex flex-row gap gap-1 items-center justify-center">
             <TimerIcon />
-            <p className="body-s">{userData.completeTime}</p>
+            <p className="body-m text-neutral-500">{formattedCompleteDate}</p>
           </div>
         </div>
       </div>
